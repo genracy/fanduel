623240c27256363c91421ebb517114a65574bc34
Andrew <genracy@gmail.com>
2017-06-24 17:06:09 +0100

    basic order count

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -45,18 +45,41 @@ app.controller('DrinkController', function($scope, jsonGetter){
         var order = [];
         var drinks = [];
 
+        $scope.order = order;
+
         jsonGetter.getDrinksForBar(101).then(function(response){
             drinks = response.data;
             $scope.drinks = drinks;
         });
 
         $scope.selectDrink = function(drink){
-            order.push(drink);
+
+            var found = false;
+            for (var o in order){
+                if(order[o].id === drink.id){
+                    order[o].quantity++;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found){
+                var item = drink;
+                item.quantity = 1;
+                    order.push(item);
+            }
         }
 
           $scope.unselectDrink = function(drink){
-                    order.pop(drink);
-                }
+            for (var o in order){
+                if(order[o].id === drink.id){
+                    if(order[o].quantity > 0){
+                        order[o].quantity--;
+                     }
+                     break;
+                 }
+             }
+          }
 
         $scope.showOrder = function(){
             console.log(order);
