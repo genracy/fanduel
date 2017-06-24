a877a38f8ed383fc70fdc29438cc0ea823b70eb4
Andrew <genracy@gmail.com>
2017-06-24 14:36:38 +0100

    adds initial bars list

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,5 @@
 var app = angular.module('PrototypeApp', [
     "ngRoute",
-    "ngTouch",
     "mobile-angular-ui"
 ]);
 
@@ -11,5 +10,21 @@ app.config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/drinks', {
         templateUrl: 'drinks.html'
     });
+});
+
+app.controller('BarController', function($rootScope, $scope) {
+
+  var bars = [];
+
+//TODO get the bar items
+  for (var i = 1; i <= 100; i++) {
+    var bar  = {
+        name: 'bar ' + i,
+        imageUrl: 'images/' + i + '.jpg'
+    }
+    bars.push(bar);
+  }
+
+  $scope.bars = bars;
 });
 
